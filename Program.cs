8074fd2830ba2f51e860fb13ac72d2c8026a1d70
gladiator06m <n.khedaiji@gmail.com>
2021-03-02 17:36:15 +0100

    commit du 02/03/2021

--- Program.cs
+++ Program.cs
@@ -5,16 +5,23 @@ namespace Projet1
     class Program
     {
 
-        
+        //FONCTION AFFICHAGE MESSAGE
+        static void afficherInfosPersonne(string nom, int age)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Bonjour, vous vous appelez " + nom + ", vous avez " + age + " ans");
+            Console.WriteLine("bientôt vous aurez " + (age + 1) + " ans");
+        }
 
-        static int DemanderAge()
+        //FONCTION AGE
+        static int DemanderAge(string nomPersonne)
         {
             int ageConvert = 0;
             while (ageConvert <= 0)
             {
                 try
                 {
-                    Console.Write("Quel est ton age? ");
+                    Console.Write("Quel est l'age de " + nomPersonne + " ");
                     string TonAge = Console.ReadLine();
                     ageConvert = int.Parse(TonAge);
 
@@ -34,14 +41,16 @@ namespace Projet1
             return ageConvert;
         }
 
-        static string DemanderNom()
+
+        //FONCTION PRÉNOM
+        static string DemanderNom(int numeroPersonne)
         {
             string TonNom = "";
 
             while (TonNom == "")
             {
 
-                Console.Write("Quel est ton nom? ");
+                Console.Write("Quel est le nom de la personne numéro " + numeroPersonne + " ? ");
                 TonNom = Console.ReadLine();
                 TonNom = TonNom.Trim();
                 if (TonNom == "")
@@ -57,23 +66,26 @@ namespace Projet1
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-
             //DEMANDER LE NOM
-            string TonNom = DemanderNom();
+            string TonNom1 = DemanderNom(1);
+            string TonNom2 = DemanderNom(2);
 
             //DEMANDER L'AGE
-            int ageConvert = DemanderAge();
+            int ageConvert1 = DemanderAge(TonNom1);
+            int ageConvert2 = DemanderAge(TonNom2);
 
-            //AFFICHER LE MESSAGE
-            ageConvert = ageConvert + 1;
-            Console.WriteLine("Vous avez " + ageConvert + " ans et vous vous appelez " + TonNom);
+            //AFFICHER LE MESSAGE METHODE DÉBUTANT
+            //Console.WriteLine();
+            //Console.WriteLine("Bonjour, vous vous appelez " + TonNom1 + ", vous avez " + ageConvert1 + " ans");
+            // Console.WriteLine("bientôt vous aurez " + (ageConvert1+1) + " ans");
 
+            //Console.WriteLine();
+            //Console.WriteLine("Bonjour, vous vous appelez " + TonNom2 + ", vous avez " + ageConvert2 + " ans");
+            //Console.WriteLine("bientôt vous aurez " + (ageConvert2 + 1) + " ans");
 
-
-
-
-
-
+            //AFFICHER LE MESSAGE METHODE PRO
+            afficherInfosPersonne(TonNom1, ageConvert1);
+            afficherInfosPersonne(TonNom2, ageConvert2);
 
         }
     }
